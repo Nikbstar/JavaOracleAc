57575ac9e8f20d780444f80dafb7b84385b780c1
Nikbstar <Nikbstar@gmail.com>
2017-10-26 16:23:51 +0500

    add lesson5

---
+++ src/ru/nik66/lesson5/incapsulation/App.java
@@ -0,0 +1,29 @@
+package ru.nik66.lesson5.incapsulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        Human human = createHumanConsole();
+        System.out.println(human);
+    }
+
+    private static Human createHumanConsole() throws IOException {
+        Human human = null;
+        // System.in - читает введенные символы побайтно.
+        // InputStreamReader() - переводит полученные байты в char
+        // BufferedReader() - Преобразует полученные чары в строку
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input human's name: ");
+        // Считать тсроку и консоли и записать в name
+        String name = br.readLine();
+        System.out.println("Input human's age: ");
+        int age = Integer.parseInt(br.readLine());
+        human = new Human(name, age);
+        return human;
+    }
+
+}
---
+++ src/ru/nik66/lesson5/incapsulation/Human.java
@@ -0,0 +1,46 @@
+package ru.nik66.lesson5.incapsulation;
+
+/**
+ * Created by 075kotkinna on 26.10.2017.
+ */
+public class Human {
+
+    public static final int MIN_AGE = 0;
+    public static final int MAX_AGE = 100;
+    private String name;
+    private int age;
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age >= MIN_AGE && age <= MAX_AGE) {
+            this.age = age;
+        } else {
+            System.out.println("Wrong input, use age from " + MIN_AGE + " to " + MAX_AGE);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Human(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+}
